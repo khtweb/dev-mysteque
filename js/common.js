8e5c38b53693971e3878c7cd290a28bdc9fe8f88
khtaok <aoki.kohta@fanplus.co.jp>
2026-04-21 17:15:58 +0900

    intial commit (roirom-mysteque)

---
+++ js/common.js
@@ -0,0 +1,43 @@
+window.addEventListener('load', loadFinished);
+function loadFinished(){
+    $('body').addClass('readed');
+
+    window.setTimeout(dispMsg, 2500);
+    function dispMsg(){
+      $('body').addClass('loaded');
+    }
+}
+
+
+(function($) {
+  $(function() {
+      // Nav Fixed
+      $(window).scroll(function() {
+          if ($(window).scrollTop() > 0) {
+              $('body').addClass('scrl');
+          } else {
+              $('body').removeClass('scrl');
+          }
+      });
+  });
+})(jQuery);
+
+
+
+function anchor() {
+    $('a[href^="#"]').click(function () {
+      var Hash = $(this.hash),
+        $Hash = $(Hash);
+      if ($Hash.length > 0) {
+        var HashOffset = $Hash.offset().top;
+        $('html,body').animate({
+          scrollTop: HashOffset
+        }, 600);
+      }
+      return false;
+    });
+}
+
+$(function(){
+	anchor();
+});
